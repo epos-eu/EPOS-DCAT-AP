0a75f4e7625d36ef66fdf6f030fbaa33c8f5d4f3
Simon Flower <smf@bgs.ac.uk>
2018-08-28 16:06:46 +0100

    More work on new RDF format metadata

--- examples/WP13/turtle_writing_code/ttl_reader.py
+++ examples/WP13/turtle_writing_code/ttl_reader.py
@@ -4,14 +4,15 @@ import ttl_utils as ttl
 g = ttl.read_ttl ('../EPOS-DCAT-AP_WP13_DDSS-001_INTERMAGNET.ttl')
 
 g2 = ttl.search_triples (g, 'mailto:smf@bgs.ac.uk')
-ttl.dump_graph (g2)
+print ("")
 
 g3 = ttl.search_triples (g, 'http://orcid.org/0000-0002-7677-5158')
-ttl.dump_graph (g3)
+print ("")
+
+ttl.dump_graph(g)
 
 g_final = g2 + g3
-ttl.dump_graph (g_final)
 
-ttl.write_ttl(g, 'output.ttl')
+ttl.write_ttl(g_final, 'output.ttl')
 
 
--- examples/WP13/turtle_writing_code/ttl_utils.py
+++ examples/WP13/turtle_writing_code/ttl_utils.py
@@ -13,13 +13,31 @@ def copy_namespaces (graph1, graph2):
     #for namespace in graph1.namespaces():
         graph2.bind (ns_prefix, ns_uri)
 
-def search_triples (graph, search_term):
-    new_g = Graph()
-    copy_namespaces (graph, new_g)
+def search_triples (graph, search_term, new_graph=None):
+    print ("Search: " + search_term)
+    if new_graph is None:
+        new_graph = Graph()
+        copy_namespaces (graph, new_graph)
+    # iterate over the the triples in the graph
     for s, p, o in graph:
-        if (str(s) == search_term) or (str(o) == search_term):
-            new_g.add ((s, p, o))
-    return new_g
+        triple = (s, p, o)
+        # is the search term the subject of the triple?
+        if str(s) == search_term:
+            # yes - have we seen this triple before?
+            if not triple in new_graph:
+                # no - recursively search for triples containing the object
+                print ("Found triple by subject: " + str(s) + ", " + str(p) + ", " + str(o))
+                new_graph.add (triple)
+                #search_triples(graph, str(o), new_graph)
+        # is the search term the object of the triple?
+        elif str(o) == search_term:
+            # yes - have we seen this triple before?
+            if not triple in new_graph:
+                # no - recursively search for triples containing the subject
+                print("Found triple by object: " + str(s) + ", " + str(p) + ", " + str(o))
+                new_graph.add (triple)
+                search_triples (graph, str(s), new_graph)
+    return new_graph
 
 def dump_graph (graph):
     print("--- printing raw triples: " + str(len(graph)) + " ---")
