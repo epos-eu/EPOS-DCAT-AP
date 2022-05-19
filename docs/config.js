7cc4a6cddb8ec4fa91a06072c248a03fff7409f5
manuelasbarra <manuela.sbarra@ingv.it>
2022-05-19 12:10:38 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -2,7 +2,7 @@ var respecConfig = {
 //	preProcess: [dfn_index],
     logos: [
    {    //src: 'https://www.epos-eu.org/themes/epos/logo.svg',
-         src: './logo.svg',
+         src: './images/logo.svg',
 
        alt: "EPOS",
 
