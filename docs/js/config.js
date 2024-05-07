9cf9f72ac27bd9ae565455ae46b2f83c9cceca21
manuelasbarra <manuela.sbarra@ingv.it>
2024-05-07 12:55:27 +0200

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -81,7 +81,7 @@ function quickreference() {
         }
       ]
       },
-      /*{
+      {
       key: "Document status:",
       data: [
       {
@@ -89,7 +89,7 @@ function quickreference() {
       }
       ]
       },
-      {
+      /*{
       key: "Document version:",
       data: [
       {
